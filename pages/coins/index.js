9a719124110ee8e6a7e30f99eab09b17f5917e4f
Bais Ali Khan <112929397+baisalikhan@users.noreply.github.com>
2023-07-13 12:29:46 +0500

    added some styling and show the data in cards

--- pages/coins/index.js
+++ pages/coins/index.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-
+import { Card, Col, Row } from "antd";
+import React from "react";
+import styles from "../../styles/coins.module.css";
+// const { Meta } = Card;
 //SSG: Static Side Generation
 //SSR: Server Side Rendering
 
@@ -7,23 +10,38 @@ const CoinList = ({ coinData }) => {
   console.log(coinData);
   return (
     <div>
-      {coinData.coins.map((coin, index) => {
-        return (
-          <div key={index}>
-            <h1>{coin.name}</h1>
-            <img src={coin.icon} />
-            <p>$ {coin.price}</p>
-          </div>
-        );
-      })}
+      <div class={styles.cards}>
+        {coinData.coins.map((coin, index) => {
+          return (
+            <section className={styles.cards__item} key={index}>
+              <Card
+                hoverable
+                className={styles.card}
+                bordered={false}
+                cover={
+                  <img
+                    alt="example"
+                    className={styles.imgStyle}
+                    src={coin.icon}
+                  />
+                }
+              >
+                <h1>{coin.name}</h1>
+                <h5>{coin.price}</h5>
+                {/* <Meta title={coin.name} description={coin.price} /> */}
+              </Card>
+            </section>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
 //getStaticProps: renders or pre-renders the page at build time
 
-// export const getStaticProps = async () => {
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
+  // export const getServerSideProps = async () => {
   // export const getStaticProps = async ({prarams}) => {
   // const id = params.id; //get id from the url
   const data = await axios.get(
